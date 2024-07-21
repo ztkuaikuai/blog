82360b5927e79fa486076b187db2cc8088d0b5fb
ztkuaikuai <2647731903@qq.com>
2024-07-21 20:59:07 +0800

    pack: 引入sanitize-html；优化RSS；

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,14 +6,20 @@ export async function GET(context) {
   const posts = Object.values(postImportResult);
   return rss({
     title: '筷筷的博客',
+    link: 'blog.kuaikuaitz.top',
     description: '我的人类使用说明书',
+    author: "筷筷",
+    commentsUrl: "https://github.com/ztkuaikuai",
     site: context.site,
     items: posts.map((post) => ({
       link: post.url,
-      content: post.compiledContent(),
+      content: sanitizeHtml(post.compiledContent(), {
+        allowedTags: false,
+        allowedAttributes: false
+      }),
       ...post.frontmatter,
     })),
-    stylesheet: '/rss/styles.xsl',
+    // stylesheet: '/rss/styles.xsl',
     customData: `<language>zh-cn</language>`,
   });
 }
