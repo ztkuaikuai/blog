72b2113cda0b04993da4899b85c9ca1d49c9c074
ztkuaikuai <2647731903@qq.com>
2024-07-21 20:22:38 +0800

    perf: try RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,18 @@
-import rss, {pagesGlobToRssItems} from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
 
 export async function GET(context) {
+  const postImportResult = import.meta.glob('./posts/*.md', { eager: true });
+  const posts = Object.values(postImportResult);
   return rss({
     title: '筷筷的博客',
     description: '我的人类使用说明书',
     site: context.site,
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./posts/*.{md,mdx}'),
-    ),
+    items: posts.map((post) => ({
+      link: post.url,
+      content: post.compiledContent(),
+      ...post.frontmatter,
+    })),
     stylesheet: '/rss/styles.xsl',
     customData: `<language>zh-cn</language>`,
   });
